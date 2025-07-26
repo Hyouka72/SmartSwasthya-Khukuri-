54980c9a2454a137730afcb5c46eb005afa82345
Hyouka45 <khewang212@gmail.com>
2025-07-26 14:33:49 +0545

    Repository done

--- Backend/src/main/java/com/backend/SmartSwasthya/Repository/PatientRepository.java
+++ Backend/src/main/java/com/backend/SmartSwasthya/Repository/PatientRepository.java
@@ -3,6 +3,7 @@ package com.backend.SmartSwasthya.Repository;
 import com.backend.SmartSwasthya.Models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
